bebddd8f73b347aa3193385a0ded060790be0a78
jaluiovilash <ovilashjalui@gmail.com>
2025-11-19 12:28:53 +0530

    made small changes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,6 @@
-// app/sitemap.ts
-import { NextResponse } from "next/server";
+export default function sitemap() {
+  const BASE_URL = "https://colossalcodes.vercel.app";
 
-const BASE_URL = "https://colossalcodes.vercel.app";
-
-export async function GET() {
   const staticPages = [
     "",
     "typescript",
@@ -15,28 +12,10 @@ export async function GET() {
     "contact",
   ];
 
-  const pages = staticPages
-    .map((path) => {
-      const url = `${BASE_URL}/${path}`.replace(/\/$/, "");
-      return `
-        <url>
-          <loc>${url}</loc>
-          <changefreq>weekly</changefreq>
-          <priority>0.8</priority>
-        </url>
-      `;
-    })
-    .join("");
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${pages}
-    </urlset>`;
-
-  return new NextResponse(xml, {
-    headers: {
-      "Content-Type": "application/xml",
-      "Cache-Control": "public, max-age=0, s-maxage=3600", // server cache for 1 hour
-    },
-  });
+  return staticPages.map((path) => ({
+    url: `${BASE_URL}/${path}`.replace(/\/$/, ""),
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 0.8,
+  }));
 }
